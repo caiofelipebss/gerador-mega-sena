463f4980df821c3a25287736fa9c511aaa811b21
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-01-05 00:09:59 -0300

    Add project files.

---
+++ SugestaoMegaSena/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SugestaoMegaSena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" **** Sugestão Mega-Sena **** ");
+            Console.WriteLine("******************************");
+            Console.WriteLine();
+
+            var randNum = new Random();
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.Write($" {randNum.Next(1, 60)} ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
